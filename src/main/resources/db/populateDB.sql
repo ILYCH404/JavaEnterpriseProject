980d2e9549e4f3228a64321ea0b23308f7344e27
Ilya Chukanov <ilyachuk77@gmail.com>
2022-02-21 13:18:44 +0300

    HW03

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,4 +1,5 @@
 DELETE FROM user_roles;
+DELETE FROM meals;
 DELETE FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
@@ -10,3 +11,12 @@ VALUES ('User', 'user@yandex.ru', 'password'),
 INSERT INTO user_roles (role, user_id)
 VALUES ('USER', 100000),
        ('ADMIN', 100001);
+
+INSERT INTO meals (user_id, datetime, description, calories)
+VALUES
+    (100000, '2020-01-30 07:00', 'Завтрак(кашка)', 800),
+    (100000, '2020-01-30 12:00', 'Обед', 1000),
+    (100000, '2020-01-30 19:00', 'Ужин', 300),
+    (100001, '2020-01-30 07:00', 'Завтрак(сырники)', 800),
+    (100001, '2020-01-30 12:00', 'Обед', 1000),
+    (100001, '2020-01-30 19:00', 'Ужин', 300);
